d178b83e6973a6370b8c239768b453f61079202b
Md Javedul Ferdous <mdjavedulferdous@gmail.com>
2021-04-27 22:05:17 -0400

    10:05

---
+++ contracts/DriverInformation.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.18;
+
+contract DriverInformation {
+   string driverid;
+   string driverName;
+   string age;
+   string dob;
+   string city;
+   string zip;
+
+function setDriverInformation(string _driverid, string _driverName, string _age, string _dob, string _city, string _zip) public {
+      driverid = _driverid;
+      driverName= _driverName;
+      age = _age;
+      dob = _dob;
+      city = _city;
+      zip = _zip;
+      
+   }
+   
+   function getDIRenewalChange() public constant returns (string,string,string,string,string,string) {
+       return ( driverid,driverName,age,dob,city,zip);
+   
+   }
+    
+}
